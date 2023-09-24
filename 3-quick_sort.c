eab48ea5bb1adf1a7b1161d7aa2fba2c969450eb
Kele <ebiritg@gmail.com>
2023-09-24 16:52:20 +0100

    Write a function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+
+/**
+ * partiton - takes an array, a range of indices, and a pivot value,
+ *				and rearranges the elements in the array
+ * @arr: An array of integers that needs to be partitioned.
+ * @small_val: the starting index of the subarray that needs
+ *				to be partitioned.
+ * @big_val: represents the index of the last element in the array.
+ * @size: represents the size of the array "arr".
+ *
+ * Return: the index of the pivot element after partitioning the array.
+ */
+size_t partiton(int *arr, int small_val, int big_val, size_t size)
+{
+	int i, j;
+	int pivot = arr[big_val];
+
+	i = small_val - 1;
+	for (j = small_val; j <= big_val - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(arr, i, j);
+		}
+	}
+	swap(arr, (i + 1), big_val);
+	print_array(arr, size);
+	return (i + 1);
+}
+
+/**
+ * sort_recursive - recursively sorts an array of integers using
+ *					the partitioning technique.
+ * @array: Pointer to the array that needs to be sorted.
+ * @small_val: The starting index of the array or subarray to be sorted.
+ * @big_val: The index of the last element in the
+ *				array that needs to be sorted.
+ * @size: Size of the array. It is of type size_t.
+ *
+ * Return: void
+ */
+void sort_recursive(int *array, int small_val, int big_val, size_t size)
+{
+	int val;
+
+	if (small_val < big_val)
+	{
+		val = partiton(array, small_val, big_val, size);
+
+		sort_recursive(array, small_val, val - 1, size);
+		sort_recursive(array, val + 1, big_val, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers using the quicksort algorithm.
+ * @array: Pointer to the first element of the array
+ * @size: The number of elements in the array.
+ *
+ * Return:Void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+	{
+		message("3-O", "n * log(n)\nn * log(n)\nn square\n");
+		return;
+	}
+	sort_recursive(array, 0, size - 1, size);
+	message("3-O", "n * log(n)\nn * log(n)\nn square\n");
+}
