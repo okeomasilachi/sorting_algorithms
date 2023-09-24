1385e6142fd2e45e98111b46177add329ad711ce
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-24 16:44:31 +0100

    add the sort.h file (header file)

---
+++ sort.h
@@ -0,0 +1,38 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+typedef unsigned int num;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void swap(int *array, size_t num1, size_t num2);
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *create_listint(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void message(const char *filename, const char *message);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+size_t partiton(int *arr, int small_val, int big_val, size_t size);
+void sort_recursive(int *array, int small_val, int big_val, size_t size);
+
+#endif /* SORT_H */
