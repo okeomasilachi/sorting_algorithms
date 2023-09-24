14fdf9a47e21d1cf3afe8a94f60295e0f3c71b61
Kele <ebiritg@gmail.com>
2023-09-24 16:48:01 +0100

    Write a function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - implements the insertion sort algorithm
+ *                  to sort a doubly linked list in ascending order.
+ * @list: Pointer to a pointer of type `listint_t`. It represents a
+ *          doubly linked list.
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *fst = NULL, *tmp = NULL;
+
+
+	if (*list == NULL || (*list)->next == NULL)
+	{
+		message("1-O", "O(n)\nn square\nn square\n");
+		return;
+	}
+
+	for (fst = (*list)->next; fst; fst = fst->next)
+	{
+		tmp = fst;
+		while (tmp->prev != NULL && tmp->prev->n > tmp->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev != NULL)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+	}
+	message("1-O", "O(n)\nn square\nn square\n");
+}
