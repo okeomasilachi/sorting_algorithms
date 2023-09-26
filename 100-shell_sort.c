8a2e014b3d65767c5f2580d17fd9330091def62c
Kele <ebiritg@gmail.com>
2023-09-26 01:56:30 +0100

    Write a function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	gap = 1;
+
+	while (gap < size / 3)
+	{
+		gap = gap * 3 + 1;
+
+	}
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j = j - gap;
+			}
+			array[j] = temp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
